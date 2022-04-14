62c01bba6c98bf05a604f1de33af760dadce2777
haosappro <haop7078@gmail.com>
2022-04-14 19:24:40 +0700

    first commit

---
+++ Product238/Product238/Product238/Product238/Product238/Controllers/ProductController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Product238.Models;
+
+namespace Product238.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>();
+        // GET: Product
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+        public ActionResult ListProducts()
+        {
+         if(products.Count() == 0)
+            {
+                products.Add(new Product(1, "Yamaha C40", "Nhạc Cụ", 4200000, "/Content/images/guitar1.jpg"));
+                products.Add(new Product(2, "Yamaha GS23", "Nhạc Cụ", 10500000, "/Content/images/guitar2.jpg"));
+                products.Add(new Product(3, "Yamaha F2", "Nhạc Cụ", 8750000, "/Content/images/guitar3.jpg"));
+                products.Add(new Product(4, "Yamaha Silent 350C", "Nhạc Cụ", 1905000, "/Content/images/guitar4.jpg"));
+                products.Add(new Product(5, "Guitar Mini Yamaha G10", "Nhạc Cụ", 5200000, "/Content/images/guitar5.jpeg"));  
+            }
+            return View(products);
+        }
+        public ActionResult EditProduct(int id)
+        {
+            Product product = new Product();
+            foreach (Product pd in products)
+            {
+                if (pd.Id == id)
+                {
+                    product = pd;
+                    break;
+                }
+            }
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("EditProduct")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult EditBook(int? id, string pName, string pType, double pPrice, string imgCover)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Product pd in products)
+            {
+                if (id == pd.Id)
+                {
+                    pd.PName = pName;
+                    pd.PType = pType;
+                    pd.PPrice = pPrice;
+                    pd.ImgCover = imgCover;
+                    break;
+                }
+            }
+            // Tạo dữ liệu cho view với ViewBag
+            ViewBag.Products = products;
+            return View("ListProducts", products);
+        }
+
+        /*----------------Delete---------------*/
+        public ActionResult DeleteProduct(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Product product = new Product();
+            foreach (var pd in products)
+            {
+                if (pd.Id == id)
+                {
+                    product = pd;
+                    break;
+                }
+            }
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("DeleteProduct")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteProductId(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Product product = new Product();
+            foreach (Product pd in products)
+            {
+                if (pd.Id == id)
+                {
+                    product = pd;
+                    break;
+                }
+            }
+            products.Remove(product);
+            return View("ListProducts", products);
+        }
+
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("CreateProduct")]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateProduct([Bind(Include = "id, PName, PType, PPrice, ImgCover")] Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Thêm sách mới
+                    products.Add(product);
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Lỗi ghi dữ liệu");
+            }
+            return View("ListProducts", products);
+        }
+
+        /*-------------Detail--------------*/
+
+        public ActionResult DetailProduct(int id)
+        {
+            Product product = new Product();
+            foreach (Product pd in products)
+            {
+                if (pd.Id == id)
+                {
+                    product = pd;
+                    break;
+                }
+            }
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+
+
+
+    }
+}
---
+++ Product238/Product238/Product238/Product238/Product238/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Product238.Models
+{
+    public class Product
+    {
+        int id;
+        string pName;
+        string pType;
+        double pPrice;
+        string imgCover;
+
+        //Propeties
+        [Required(ErrorMessage = "ID không được trống")]
+        [Display(Name = "ID")]
+        public int Id { get => id; set => id = value; }
+        // Kiểm tra ràng buộc cho tiêu đề : không trống và dài <= 250
+        [Required(ErrorMessage = "Tên sản phẩm không được trống")]
+        [StringLength(250, ErrorMessage = "Tên sản phẩm không quá 250 ký tự")]
+        [Display(Name = "Tên sản phẩm")]
+        public string PName { get => pName; set => pName = value;}
+        [Required(ErrorMessage = "Thể loại không được trống")]
+        [Display(Name = "Thể loại")]
+        public string PType { get => pType; set => pType = value;}
+
+        [Required(ErrorMessage = "Giá bán phải lớn hơn 0")]
+        [Display(Name = "Giá bán")]
+        public double PPrice { get => pPrice; set => pPrice = value;}
+        [Display(Name = "Hình ảnh")]
+        public string ImgCover { get => imgCover; set => imgCover = value;}    
+
+        //Constructors
+        public Product() { }
+        public Product(int id, string pName, string pType, double pPrice, string imgCover)
+        {
+            this.id = id;
+            this.pName = pName;
+            this.pType = pType;
+            this.pPrice = pPrice;
+            this.imgCover = imgCover;
+        }
+    }
+}
